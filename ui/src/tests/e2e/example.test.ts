b538176593824775bcd02cd2c4eb9d12ffa1fe9a
Vikrant Pradhan <pradhan.vikrant.17@gmail.com>
2025-08-14 12:22:20 +0530

    Complete transition of frontend to new template

---
+++ ui/src/tests/e2e/example.test.ts
@@ -0,0 +1,51 @@
+import {
+  test,
+  expect,
+  _electron as electron,
+  ElectronApplication,
+  Page,
+} from "@playwright/test";
+import { Input } from "electron";
+import { findLatestBuild, parseElectronApp } from "electron-playwright-helpers";
+
+/*
+ * Using Playwright with Electron:
+ * https://www.electronjs.org/pt/docs/latest/tutorial/automated-testing#using-playwright
+ */
+
+let electronApp: ElectronApplication;
+
+test.beforeAll(async () => {
+  const latestBuild = findLatestBuild();
+  const appInfo = parseElectronApp(latestBuild);
+  process.env.CI = "e2e";
+
+  electronApp = await electron.launch({
+    args: [appInfo.main],
+  });
+  electronApp.on("window", async (page) => {
+    const filename = page.url()?.split("/").pop();
+    console.log(`Window opened: ${filename}`);
+
+    page.on("pageerror", (error) => {
+      console.error(error);
+    });
+    page.on("console", (msg) => {
+      console.log(msg.text());
+    });
+  });
+});
+
+test("renders the first page", async () => {
+  const page: Page = await electronApp.firstWindow();
+  const input = await page.waitForSelector("input");
+  const placeholder = await input.getAttribute("placeholder");
+  expect(placeholder).toBe("Type to search...");
+
+});
+
+test("renders page name", async () => {
+  const page: Page = await electronApp.firstWindow();
+  const answer = page.locator("#answer");
+  await expect(answer).toContainText('Lorem')
+});

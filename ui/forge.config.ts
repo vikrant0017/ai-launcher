6236d1cfe93c25f7565b7b0f5ae67b3cd4acc18f
Vikrant Pradhan <pradhan.vikrant@hotmail.com>
2025-10-01 13:51:26 +0530

    Add the extraResrouce config

--- ui/forge.config.ts
+++ ui/forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: "../backend/dist/server", // goes to <appname>/resources/server
   },
   rebuildConfig: {},
   makers: [
